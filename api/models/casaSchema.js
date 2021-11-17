1af6b8d627811aac4f8ebb5dad2a39a2b6e5e380
felipepadua011 <89053801+felipepadua011@users.noreply.github.com>
2021-11-17 20:15:07 -0300

    Schema Casas

--- api/models/casaSchema.js
+++ api/models/casaSchema.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-    tipo: 
-    localizacao:
-    valor:
-    imagemURL
-})
+    tipo: { type: String, required: true},
+    localizacao: { type: String, required: true},
+    valor: { type: Number, required: true},
+    imagemURL: { type: String, required: true},
+});
+
+module.exports = mongoose.model("casas" , schema);
