a2b4468238154abdd77a75d2af028dcb9aea5855
Nikolas Furuta <nikolasfuruta@gmail.com>
2021-11-17 21:09:13 -0300

    bugFix add

--- api/routes/casaRoutes.js
+++ api/routes/casaRoutes.js
@@ -2,10 +2,10 @@ const CasaController = require('')
 const { Router } = require('express');
 const router = Router()
 
-router.get('/listall', CasaController.listarTudo)
-router.get('/listid/:id', CasaController.buscaPorId)
+// router.get('/listall', CasaController.listarTudo)
+// router.get('/listid/:id', CasaController.buscaPorId)
 router.post('/add', CasaController.adicionar)
-router.put('/update/:id', CasaController.atualizar)
-router.delete('/delete/:id', CasaController.apagar )
+// router.put('/update/:id', CasaController.atualizar)
+// router.delete('/delete/:id', CasaController.apagar )
 
 module.exports = router;
