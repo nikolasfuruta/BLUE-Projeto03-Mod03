7112eb80e322e6bf1a2c717bede2adca345a7375
felipepadua011 <89053801+felipepadua011@users.noreply.github.com>
2021-11-17 21:33:56 -0300

    Mongo Add

--- api/models/conn/index.js
+++ api/models/conn/index.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 async function Conn(){
-    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_BASE}`,{ // string de conexao ou local ou atlas
-        useNewUrlParser: true,
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_BASE}`,{ 
         useUnifiedTopology: true,
     }).then(() => { 
         console.log("MongoDB esta conectado");
