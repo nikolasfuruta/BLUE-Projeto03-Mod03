43af781031689cc7ab5c2eae8d6c66a9be2d776b
Nikolas Furuta <nikolasfuruta@gmail.com>
2021-11-17 22:41:28 -0300

    rota Casa concluído

--- api/controllers/casaController.js
+++ api/controllers/casaController.js
@@ -27,7 +27,7 @@ exports.adicionar = async (req,res) => {
 exports.listarTudo = async (req,res) => {
     try{
         const result = await Casa.find({})
-        return res.status(201).json(result)
+        return res.status(200).json(result)
     } catch(err){
         console.error(err.message)
         return res.status(400).json({message:"ERRO"})
@@ -41,7 +41,48 @@ exports.buscaPorId = async (req,res) => {
             return
         }
         const result = await Casa.findById({_id:req.params.id})
-        return res.status(201).json(result)
+        return res.status(200).json(result)
+    } catch(err){
+        console.error(err.message)
+        return res.status(400).json({message:"ERRO"})
+    }
+}
+
+exports.apagar = async (req,res) => {
+    try{
+        if(req.params.id.length != 24){
+            res.status(400).json({message: "Parâmetro incorreto"});
+            return
+        }
+        await Casa.findByIdAndDelete({_id:req.params.id})
+        return res.status(200).json({message:"Deletado com sucesso"})
+    } catch(err){
+        console.error(err.message)
+        return res.status(400).json({message:"ERRO"})
+    }
+}
+
+exports.atualizar = async (req,res) => {
+    try{
+        if(req.params.id.length != 24){
+            res.status(400).json({message: "Parâmetro incorreto"});
+            return
+        }
+        if(!req.body.tipo){
+            res.status(400).json({message: "Tipo esta vazio"});
+            return;
+        } else if (!req.body.localizacao){
+            res.status(400).json({message: "Localização esta vazia"});
+            return;
+        } else if (!req.body.valor){
+            res.status(400).json({message: "Valor esta vazio"});
+            return;
+        } else if(!req.body.imagemUrl){
+            res.status(400).json({message: "URL da imagem esta vazio"});
+            return;
+        };
+        const result = await Casa.findByIdAndUpdate({_id:req.params.id}, req.body)
+        return res.status(200).json({message:"Atualizado com sucesso"})
     } catch(err){
         console.error(err.message)
         return res.status(400).json({message:"ERRO"})
--- api/routes/casaRoutes.js
+++ api/routes/casaRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 router.get('/listall', CasaController.listarTudo)
 router.get('/listid/:id', CasaController.buscaPorId)
 router.post('/add', CasaController.adicionar)
-// router.put('/update/:id', CasaController.atualizar)
-// router.delete('/delete/:id', CasaController.apagar )
+router.put('/update/:id', CasaController.atualizar)
+router.delete('/delete/:id', CasaController.apagar )
 
 module.exports = router;
